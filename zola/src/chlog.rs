41a0b88ef74c3bd5a41b373b3a72709e5bae18e6
Jacob Bridges <him@jaocbandkate143.com>
2020-05-28 21:57:48 -0500

    use correct #devlog channel

--- zola/src/chlog.rs
+++ zola/src/chlog.rs
@@ -3,5 +3,5 @@ use serenity::http::client::Http;
 use std::fmt::Display;
 
 pub fn devlog(http: impl AsRef<Http>, content: impl Display) {
-  let _ = ChannelId(428757534711742464).say(&http, content);
+  let _ = ChannelId(428757417535209472).say(&http, content);
 }
